d89230a1022bedd2a2660b649cd2c4756426aa8e
Hayde Martinez <hayde@reverscore.com>
2018-07-15 23:06:16 -0500

    Archivos digit recognition

--- digitnn.py
+++ digitnn.py
@@ -1,7 +1,6 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
-import time
 from collections import Counter
 
 from matplotlib import style
@@ -9,10 +8,9 @@ style.use("ggplot")
 
 def createExamples():
     numberArrayExamples = open('numArEx.txt','a')
-    numbersWeHave = range(1,10)
+    numbersWeHave = range(0,10)
     for eachNum in numbersWeHave:
-        for furtherNum in numbersWeHave:
-
+        for furtherNum in range(1,10):
             imgFilePath = 'images/numbers/'+str(eachNum)+'.'+str(furtherNum)+'.png'
             ei = Image.open(imgFilePath)
             eiar = np.array(ei)
@@ -36,7 +34,9 @@ def whatNumIsThis(filePath):
             currentNum = splitEx[0]
             currentAr = splitEx[1]
             eachPixEx = currentAr.split('],')
+            print(eachPixEx)
             eachPixInQ = inQuestion.split('],')
+            print(eachPixInQ)
             x = 0
             while x < len(eachPixEx):
                 if eachPixEx[x] == eachPixInQ[x]:
@@ -45,7 +45,7 @@ def whatNumIsThis(filePath):
                 x+=1
         except Exception as e:
             print(str(e))
-                
+
     x = Counter(matchedAr)
     print(x)
     graphX = []
@@ -63,16 +63,16 @@ def whatNumIsThis(filePath):
     fig = plt.figure()
     ax1 = plt.subplot2grid((4,4),(0,0), rowspan=1, colspan=4)
     ax2 = plt.subplot2grid((4,4),(1,0), rowspan=3,colspan=4)
-    
+
     ax1.imshow(iar)
     ax2.bar(graphX,graphY,align='center')
-    plt.ylim(400)
-    
+    plt.ylim(100)
+
     xloc = plt.MaxNLocator(12)
     ax2.xaxis.set_major_locator(xloc)
 
     plt.show()
-    
-#createExamples()
+
+createExamples()
 #whatNumIsThis('images/test.png')
-whatNumIsThis('images/numbers/4.5.png')
+whatNumIsThis('images/test2.png')
